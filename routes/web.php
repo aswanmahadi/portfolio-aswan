4710393fe6a49e6d343207c4827a8748273ed808
Muhammad Aswan <muhaswan190897@gmail.com>
2022-06-30 13:37:27 +0800

    npm run build

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('frontend/layouts/base');
+    return view('frontend.layouts.base');
 });
 
 Route::middleware([
